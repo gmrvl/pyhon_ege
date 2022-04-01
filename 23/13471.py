ddecda96377a9ca13dab37813e8c6439b2798dce
gmrvl <gmrvl@mail.ru>
2022-04-01 16:08:46 +0300

    ОЛЯ - 1.04 - 15

--- 23/13471.py
+++ 23/13471.py
@@ -1,10 +1,12 @@
 def k(x, y):
     if x > y or y == 24:
         return 0
-    if x == y:
+    elif x == y:
         return 1
-    if y % 2 != 0:
-        return k(x, y - 1) + k(x, y - 1 // 2)
-    return k(x, y - 1)
-print(k(1, 3))
+
+    elif y % 2 != 0:
+        return k(x, y - 1) + k(x, (y - 1) // 2)
+    else:
+        return k(x, y - 1)
+print(k(1, 25))
 
